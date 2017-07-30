adbf14d3499a31b15d2ac5dca5e12ce319864231
Thomas Bolt <t.bolt@bolt-tc.com>
2017-07-30 15:04:19 +0200

    Application-Insight Plugin added

--- wp-config.php
+++ wp-config.php
@@ -106,6 +106,7 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
+/* Domain-Mapping */
 define('SUNRISE', 'on');
 
 /* That's all, stop editing! Happy blogging. */

7fce3d63cf4028575e8bc4cf90110261aa1075d6
Thomas Bolt <t.bolt@bolt-tc.com>
2017-07-30 02:37:03 +0200

    configure multi-domain

--- wp-config.php
+++ wp-config.php
@@ -98,13 +98,15 @@ $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
 
 /* Multisite */
-define( 'WP_ALLOW_MULTISITE', true );
+define('WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
 define('DOMAIN_CURRENT_SITE', 'bolt-tc.com');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
+
+define('SUNRISE', 'on');
 
 /* That's all, stop editing! Happy blogging. */
 
